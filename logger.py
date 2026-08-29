1ee168689caf3046a3d4983edabb5532cdb9da06
ApTest-Coder <bsailen59@gmail.com>
2026-08-29 16:57:10 +0530

    Add concise redacted logging

--- logger.py
+++ logger.py
@@ -1,6 +1,11 @@
-# Lightweight application logging.
-
 import logging
+import re
+
+class Redact(logging.Filter):
+    def filter(self, record):
+        record.msg = re.sub(r"(?i)(session|secret|token)\s*[:=]\s*[^\s]+", r"\1=[REDACTED]", str(record.msg))
+        return True
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 log = logging.getLogger("taskbot")
+log.addFilter(Redact())
